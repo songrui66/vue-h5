02abd0e2d3d40153aeda438e96139dd556b2e76b
songrui66 <939214476@qq.com>
2020-02-22 17:57:13 +0800

    配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,48 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// import store from './../store'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld,
+      meta: {
+        title: '首页'
+      }
+      // component: resolve => {
+      //   require(['@/views/login/login.vue'], resolve)
+      // }
     }
+    // {
+    //   path: '/insure',
+    //   name: 'insure',
+    //   // component: insure,
+    //   meta: {
+    //     title: '首页'
+    //   },
+    //   component: resolve => {
+    //     require(['@/views/insure/insure.vue'], resolve)
+    //   }
+    // }
   ]
 })
+
+// require(['@/views/login/login.vue'], resolve)
+// require(['./../components/HelloWorld.vue'], resolve)
+
+// Router.beforeEach((to, from, next) => {
+//   store.dispatch('openLoading')
+//   if (to.meta.title) {
+//     document.title = to.meta.title
+//   }
+//   next()
+// })
+
+// router.afterEach(to => store.dispatch('closeLoading'))
+
+export default router
