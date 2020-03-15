980ece62a66bd45d1719f99673cdd780bcb1a5ef
songrui66 <939214476@qq.com>
2020-03-15 18:03:14 +0800

    全屏样式修改

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,20 @@ export default {
 </script>
 
 <style>
+html{
+  height: 100%;
+}
+body{
+  height: 100%;
+  margin: 0;
+  background-color: #fff;
+}
 #app {
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
